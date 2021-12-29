09d81dc8311855459633be3a1a03f5bf04fab766
leomet07 <leomet07@gmail.com>
2021-12-29 16:36:10 -0500

    Added navbar to navigate policies

--- pages/policies.vue
+++ pages/policies.vue
@@ -1,21 +1,46 @@
 <template>
     <main>
         <h1 id="title">Our Commitment</h1>
-        <div>
-            <h1>Flexibility Policies:</h1>
 
-            <div
-                v-for="policy in policies"
-                v-if="policy.catagory == 'flexibility'"
-                :key="policies._id"
-            >
-                <Policy
-                    :name="policy.name"
-                    :description="policy.description"
-                    :why="policy.why"
-                    :how="policy.how"
-                />
-            </div>
+        <nav id="policy_nav">
+            <span v-on:click="scrollTo('flexibility')">Flexibility</span>
+            <span v-on:click="scrollTo('inclusivity')">Inclusivity</span>
+            <span v-on:click="scrollTo('collaboration')">Collaboration</span>
+            <span v-on:click="scrollTo('communication')">Communication</span>
+        </nav>
+
+        <div id="policies">
+            <section ref="flexibility">
+                <h1>Flexibility Policies</h1>
+
+                <div
+                    v-for="policy in policies"
+                    v-if="policy.catagory == 'flexibility'"
+                    :key="policies._id"
+                >
+                    <Policy
+                        :name="policy.name"
+                        :description="policy.description"
+                        :why="policy.why"
+                        :how="policy.how"
+                    />
+                </div>
+            </section>
+            <section ref="inclusivity">
+                <h1>Inclusivity Policies</h1>
+                <div
+                    v-for="policy in policies"
+                    v-if="policy.catagory == 'inclusivity'"
+                    :key="policies._id"
+                >
+                    <Policy
+                        :name="policy.name"
+                        :description="policy.description"
+                        :why="policy.why"
+                        :how="policy.how"
+                    />
+                </div>
+            </section>
         </div>
     </main>
 </template>
@@ -30,6 +55,11 @@ export default {
     },
     mounted() {
         console.log("Policies: ", JSON.parse(JSON.stringify(this.policies)));
+    },
+    methods: {
+        scrollTo(where) {
+            this.$refs[where].scrollIntoView();
+        },
     },
 };
 </script>
@@ -47,5 +77,27 @@ main {
 
 p {
     font-size: 1.2rem;
+}
+
+#policy_nav {
+    /* background-color: red; */
+    width: 100%;
+    height: 5vh;
+    display: flex;
+    align-items: center; /* Align center on Y axis */
+    justify-content: center; /* Align center on X axis */
+}
+
+#policy_nav span {
+    font-size: 1.4rem;
+    margin-right: 0.1vw;
+    margin-left: 0.1vw;
+    width: 15vw;
+    text-align: center;
+    text-decoration: underline;
+}
+
+#policies h1 {
+    text-align: center;
 }
 </style>
