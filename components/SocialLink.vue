462ad952d17bc414b4b924b702b7d4673fe8b75a
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-12-30 15:37:26 -0500

    Add socials page
    Created SocialLink component, which takes in flavor text, a profile URL,
    a logo URL, alt text for the image, and a --color CSS variable for a
    gradient or color for the component background.

---
+++ components/SocialLink.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="social" :style="theme">
+        <a :href="url">
+            <nuxt-img class="social_logo" :src="logo" :alt="logo_alt" />
+            {{flavor}}
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SocialLink",
+    props: ["flavor", "url", "logo", "logo_alt", "theme"]
+}
+</script>
+
+<style scoped>
+
+
+.social {
+    border: 1px solid black;
+    border-radius: 30px;
+    margin: 2vh auto;
+    padding: 16px;
+    width: 50vw;
+    color: black;
+    align-content: center;
+    font-family: "Open Sans";
+
+}
+
+.social:hover {
+    transition: 1s;
+    transform: translateX(2vw);
+    background: var(--color);
+    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+}
+
+.social:hover a {
+    transition: 1s;
+    color: white;
+}
+
+.social:hover img {
+    transition: 1s;
+    filter: invert(1);
+}
+
+.social_logo {
+    height: 32px;
+    vertical-align: middle;
+    padding-right: 12px;
+}
+
+.social a {
+    text-decoration: none;
+    color: inherit;
+}
+
+</style>
--- pages/socials.vue
+++ pages/socials.vue
@@ -1,14 +1,40 @@
 <template>
     <main>
         <h1 id="title">Our Social Media</h1>
+        <br />
+        <SocialLink
+            flavor="Follow us on Instagram"
+            url="https://www.instagram.com/xian_yingticket/"
+            logo="/img/social_media/instagram.png"
+            logo_alt="Instagram Logo"
+            theme="--color: linear-gradient(90deg, #ffde51, #f63838, #c938c5); --border: linear-gradient(90deg, #ffde51, #f63838, #c938c5)"
+        />
+        <SocialLink
+            flavor="Find us on Facebook"
+            url="https://www.facebook.com/voteforandyfin2022/"
+            logo="/img/social_media/facebook.png"
+            logo_alt="Facebook Logo"
+            theme="--color: #1877F2; --border: image(#1877F2)"
+        />
+        <SocialLink
+            flavor="Join our Discord server"
+            url="https://discord.gg/"
+            logo="/img/social_media/discord.svg"
+            logo_alt="Discord Logo"
+            theme="--color: #5865F2; --border: image(#5865F2)"
+        />
     </main>
 </template>
 
 <script>
-export default {};
+import SocialLink from "~/components/SocialLink";
+export default {
+    components: {SocialLink}
+};
 </script>
 <style scoped>
-#title {
+main {
     text-align: center;
+    align-content: center;
 }
 </style>
