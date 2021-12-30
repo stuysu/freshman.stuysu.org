d1f1e2c01e12b9178ac31f0c3f7dd65f2d051179
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-12-30 16:37:33 -0500

    Remove lingering CSS variables
    The `--border` argument was a separate argument due to different
    conversions being needed for gradients and colors in a previous
    revision, but it was removed from usage before deployment.

--- pages/socials.vue
+++ pages/socials.vue
@@ -7,21 +7,21 @@
             url="https://www.instagram.com/xian_yingticket/"
             logo="/img/social_media/instagram.png"
             logo_alt="Instagram Logo"
-            theme="--color: linear-gradient(90deg, #ffde51, #f63838, #c938c5); --border: linear-gradient(90deg, #ffde51, #f63838, #c938c5)"
+            theme="--color: linear-gradient(90deg, #ffde51, #f63838, #c938c5)"
         />
         <SocialLink
             flavor="Find us on Facebook"
             url="https://www.facebook.com/voteforandyfin2022/"
             logo="/img/social_media/facebook.png"
             logo_alt="Facebook Logo"
-            theme="--color: #1877F2; --border: image(#1877F2)"
+            theme="--color: #1877F2"
         />
         <SocialLink
             flavor="Join our Discord server"
             url="https://discord.gg/"
             logo="/img/social_media/discord.svg"
             logo_alt="Discord Logo"
-            theme="--color: #5865F2; --border: image(#5865F2)"
+            theme="--color: #5865F2"
         />
     </main>
 </template>
