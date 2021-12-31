9d8a8ff796f34cc4a559cb10bd39d6f0e09b15fb
David Chen <45245358+TheEgghead27@users.noreply.github.com>
2021-12-30 22:33:46 -0500

    Update SocialLink.vue styling

--- components/SocialLink.vue
+++ components/SocialLink.vue
@@ -18,48 +18,47 @@ export default {
 
 
 .social {
+    color: black;
     border: 1px solid black;
     border-radius: 30px;
     margin: 2vh auto;
     padding: 16px;
     width: 50vw;
-    color: black;
     text-align: center;
     align-content: center;
     font-family: "Open Sans";
-
+    transition: 1s;
 }
 
 .social:hover {
-    transition: 1s;
-    transform: translateX(1vw);
     background: var(--color);
     box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-}
-
-.social:hover a {
-    transition: 1s;
-    color: white;
-}
-
-.social:hover img {
-    transition: 1s;
-    filter: invert(1);
-}
-
-.social_logo {
-    height: 32px;
-    vertical-align: middle;
-    padding-right: 12px;
+    transform: translateX(1vw);
 }
 
 .social a {
-    text-decoration: none;
     color: inherit;
     display: inline-block;
     height: 100%;
     width: 100%;
     padding: 0;
+    text-decoration: none;
+}
+
+.social:hover a {
+    color: white;  /* if inherited, has a delay in color change */
+    transition: 1s;
+}
+
+.social:hover img {
+    filter: invert(1);
+    transition: 1s;
+}
+
+.social_logo {
+    height: 32px;
+    padding-right: 12px;
+    vertical-align: middle;
 }
 
 </style>
