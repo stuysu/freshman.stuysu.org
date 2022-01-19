52396a93ec673bc5eacdeae1c940defb26abd9e1
leomet07 <leomet07@gmail.com>
2022-01-19 13:54:53 -0500

    Start of layout development

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,19 @@
         <h1 id="title">
             Stuyvesant's Freshman Caucus, led by Andy Xian and Fin Ying!
         </h1>
+        <div id="layoutcontainer">
+            <div id="main_content"></div>
+            <div id="feed"></div>
+            <section id="news_container">
+                <div id="news">
+                    <h2>News</h2>
+                    <div id="newslist">
+                        <div class="newsevent">Uno</div>
+                        <div class="newsevent">Dos</div>
+                    </div>
+                </div>
+            </section>
+        </div>
     </main>
 </template>
 
@@ -17,5 +30,37 @@ export default {};
 main {
     width: 95vw;
     margin: auto;
+}
+
+#layoutcontainer {
+    height: 85vh;
+    background-color: grey;
+    display: flex;
+}
+
+#main_content {
+    width: 50vw;
+    height: 100%;
+    background-color: red;
+}
+#feed {
+    height: 100%;
+    width: 30vw;
+    background-color: blue;
+}
+#news_container {
+    width: 20vw;
+    height: 100%;
+    background-color: orange;
+    color: black;
+}
+#news {
+    width: 18vw;
+    margin: auto;
+    text-align: center;
+}
+.newsevent {
+    font-size: 1.4rem;
+    text-align: left;
 }
 </style>
