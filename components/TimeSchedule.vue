4da470cde9e5a434c94c3204fa12007876e444fb
leomet07 <leomet07@gmail.com>
2022-02-05 14:38:18 -0500

    Added schedule functionality

--- components/TimeSchedule.vue
+++ components/TimeSchedule.vue
@@ -1,8 +1,13 @@
 <template>
     <div id="time_schedule">
         <h1>Time Schedule</h1>
-        <h2 v-if="current">{{ current.toLocaleTimeString() }}</h2>
-        <h2 v-else>XX:XX:XX AM</h2>
+        <div v-if="current">
+            <h2>{{ current.toLocaleTimeString() }}</h2>
+            <h2>{{ schedule[period_index].name }}</h2>
+        </div>
+        <div v-else>
+            <h2>XX:XX:XX AM</h2>
+        </div>
     </div>
 </template>
 
@@ -18,7 +23,7 @@ function timeToSeconds(time) {
     seconds = Number(seconds) + Number(minutes) * 60 + Number(hours) * 3600;
     return seconds;
 }
-function get_current_period(current_seconds) {
+function get_current_period(current_seconds, schedule) {
     for (let i = 0; i < schedule.length; i++) {
         const period = schedule[i];
         if (current_seconds < timeToSeconds(period.end)) {
@@ -26,24 +31,121 @@ function get_current_period(current_seconds) {
         }
     }
 }
-const schedule = [
-    {
-        start: "00:00:00",
-        end: "7:59:59",
-        name: "Before School",
-    },
-    {
-        start: "8:00:00",
-        end: "23:59:59",
-        name: "After School",
-    },
-];
+
 export default {
     name: "TimeSchedule",
     data() {
         return {
             interval: null,
             current: null,
+            period_index: null,
+            schedule: [
+                {
+                    start: "00:00:00",
+                    end: "7:59:59",
+                    name: "Before School",
+                },
+                {
+                    start: "08:00:00",
+                    end: "08:39:59",
+                    name: "Period 1",
+                },
+                {
+                    start: "08:41:00",
+                    end: "08:44:59",
+                    name: "Periods 1-2 Passing",
+                },
+                {
+                    start: "08:45:00",
+                    end: "09:25:59",
+                    name: "Period 2",
+                },
+                {
+                    start: "09:26:00",
+                    end: "09:30:59",
+                    name: "Periods 2-3 Passing",
+                },
+                {
+                    start: "09:31:00",
+                    end: "10:14:59",
+                    name: "Period 3",
+                },
+                {
+                    start: "10:15:00",
+                    end: "10:19:59",
+                    name: "Periods 3-4 Passing",
+                },
+                {
+                    start: "10:20:00",
+                    end: "11:00:59",
+                    name: "Period 4",
+                },
+                {
+                    start: "11:01:00",
+                    end: "11:05:59",
+                    name: "Periods 4-5 Passing",
+                },
+                {
+                    start: "11:06:00",
+                    end: "11:46:59",
+                    name: "Period 5",
+                },
+                {
+                    start: "11:47:00",
+                    end: "11:51:59",
+                    name: "Periods 5-6 Passing",
+                },
+                {
+                    start: "11:52:00",
+                    end: "12:32:59",
+                    name: "Period 6",
+                },
+                {
+                    start: "12:33:00",
+                    end: "12:37:59",
+                    name: "Periods 6-7 Passing",
+                },
+                {
+                    start: "12:38:00",
+                    end: "13:18:59",
+                    name: "Period 7",
+                },
+                {
+                    start: "13:19:00",
+                    end: "13:23:59",
+                    name: "Periods 7-8 Passing",
+                },
+                {
+                    start: "13:24:00",
+                    end: "14:04:59",
+                    name: "Period 8",
+                },
+                {
+                    start: "14:05:00",
+                    end: "14:08:59",
+                    name: "Periods 8-9 Passing",
+                },
+                {
+                    start: "14:09:00",
+                    end: "14:49:59",
+                    name: "Period 9",
+                },
+                {
+                    start: "14:50:00",
+                    end: "14:53:59",
+                    name: "Periods 9-10 Passing",
+                },
+                {
+                    start: "14:54:00",
+                    end: "15:34:59",
+                    name: "Period 10",
+                },
+                {
+                    start: "15:35:00",
+                    end: "23:59:59",
+                    name: "After School",
+                },
+            ],
         };
     },
     mounted() {
@@ -63,8 +165,14 @@ export default {
             const current_seconds = timeToSeconds(
                 this.current.toLocaleTimeString()
             );
-            const period = get_current_period(current_seconds);
-            console.log("Current period:", period);
+            this.period_index = get_current_period(
+                current_seconds,
+                this.schedule
+            );
+            // console.log(
+            //     "Current period:",
+            //     JSON.parse(JSON.stringify(this.schedule[this.period_index]))
+            // );
         },
     },
 };
