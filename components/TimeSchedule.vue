8125d5a1dae0c9be3c5a22156abc5d7b8842a3bd
leomet07 <leomet07@gmail.com>
2022-02-05 22:33:07 -0500

    Table of times in html + second elapsed/remaining math

--- components/TimeSchedule.vue
+++ components/TimeSchedule.vue
@@ -4,6 +4,23 @@
         <div v-if="current">
             <h2>{{ current.toLocaleTimeString() }}</h2>
             <h2>{{ schedule[period_index].name }}</h2>
+            <div id="time_table_container">
+                <table id="time_table">
+                    <tr
+                        v-for="period in periods_only"
+                        :key="period.name"
+                        v-bind:class="
+                            period.name == schedule[period_index].name
+                                ? 'current_period'
+                                : ''
+                        "
+                    >
+                        <td>{{ period.name }}</td>
+                        <td>{{ period.start }}</td>
+                        <td>{{ period.end }}</td>
+                    </tr>
+                </table>
+            </div>
         </div>
         <div v-else>
             <h2>XX:XX:XX AM</h2>
@@ -31,6 +48,114 @@ function get_current_period(current_seconds, schedule) {
     }
 }
 
+const schedule = [
+    {
+        start: "00:00:00",
+        end: "7:59:59",
+        name: "Before School",
+    },
+    {
+        start: "08:00:00",
+        end: "08:39:59",
+        name: "Period 1",
+    },
+    {
+        start: "08:41:00",
+        end: "08:44:59",
+        name: "Periods 1-2 Passing",
+    },
+    {
+        start: "08:45:00",
+        end: "09:25:59",
+        name: "Period 2",
+    },
+    {
+        start: "09:26:00",
+        end: "09:30:59",
+        name: "Periods 2-3 Passing",
+    },
+    {
+        start: "09:31:00",
+        end: "10:14:59",
+        name: "Period 3",
+    },
+    {
+        start: "10:15:00",
+        end: "10:19:59",
+        name: "Periods 3-4 Passing",
+    },
+    {
+        start: "10:20:00",
+        end: "11:00:59",
+        name: "Period 4",
+    },
+    {
+        start: "11:01:00",
+        end: "11:05:59",
+        name: "Periods 4-5 Passing",
+    },
+    {
+        start: "11:06:00",
+        end: "11:46:59",
+        name: "Period 5",
+    },
+    {
+        start: "11:47:00",
+        end: "11:51:59",
+        name: "Periods 5-6 Passing",
+    },
+    {
+        start: "11:52:00",
+        end: "12:32:59",
+        name: "Period 6",
+    },
+    {
+        start: "12:33:00",
+        end: "12:37:59",
+        name: "Periods 6-7 Passing",
+    },
+    {
+        start: "12:38:00",
+        end: "13:18:59",
+        name: "Period 7",
+    },
+    {
+        start: "13:19:00",
+        end: "13:23:59",
+        name: "Periods 7-8 Passing",
+    },
+    {
+        start: "13:24:00",
+        end: "14:04:59",
+        name: "Period 8",
+    },
+    {
+        start: "14:05:00",
+        end: "14:08:59",
+        name: "Periods 8-9 Passing",
+    },
+    {
+        start: "14:09:00",
+        end: "14:49:59",
+        name: "Period 9",
+    },
+    {
+        start: "14:50:00",
+        end: "14:53:59",
+        name: "Periods 9-10 Passing",
+    },
+    {
+        start: "14:54:00",
+        end: "15:34:59",
+        name: "Period 10",
+    },
+    {
+        start: "15:35:00",
+        end: "23:59:59",
+        name: "After School",
+    },
+];
+
 export default {
     name: "TimeSchedule",
     data() {
@@ -38,113 +163,8 @@ export default {
             interval: null,
             current: null,
             period_index: null,
-            schedule: [
-                {
-                    start: "00:00:00",
-                    end: "7:59:59",
-                    name: "Before School",
-                },
-                {
-                    start: "08:00:00",
-                    end: "08:39:59",
-                    name: "Period 1",
-                },
-                {
-                    start: "08:41:00",
-                    end: "08:44:59",
-                    name: "Periods 1-2 Passing",
-                },
-                {
-                    start: "08:45:00",
-                    end: "09:25:59",
-                    name: "Period 2",
-                },
-                {
-                    start: "09:26:00",
-                    end: "09:30:59",
-                    name: "Periods 2-3 Passing",
-                },
-                {
-                    start: "09:31:00",
-                    end: "10:14:59",
-                    name: "Period 3",
-                },
-                {
-                    start: "10:15:00",
-                    end: "10:19:59",
-                    name: "Periods 3-4 Passing",
-                },
-                {
-                    start: "10:20:00",
-                    end: "11:00:59",
-                    name: "Period 4",
-                },
-                {
-                    start: "11:01:00",
-                    end: "11:05:59",
-                    name: "Periods 4-5 Passing",
-                },
-                {
-                    start: "11:06:00",
-                    end: "11:46:59",
-                    name: "Period 5",
-                },
-                {
-                    start: "11:47:00",
-                    end: "11:51:59",
-                    name: "Periods 5-6 Passing",
-                },
-                {
-                    start: "11:52:00",
-                    end: "12:32:59",
-                    name: "Period 6",
-                },
-                {
-                    start: "12:33:00",
-                    end: "12:37:59",
-                    name: "Periods 6-7 Passing",
-                },
-                {
-                    start: "12:38:00",
-                    end: "13:18:59",
-                    name: "Period 7",
-                },
-                {
-                    start: "13:19:00",
-                    end: "13:23:59",
-                    name: "Periods 7-8 Passing",
-                },
-                {
-                    start: "13:24:00",
-                    end: "14:04:59",
-                    name: "Period 8",
-                },
-                {
-                    start: "14:05:00",
-                    end: "14:08:59",
-                    name: "Periods 8-9 Passing",
-                },
-                {
-                    start: "14:09:00",
-                    end: "14:49:59",
-                    name: "Period 9",
-                },
-                {
-                    start: "14:50:00",
-                    end: "14:53:59",
-                    name: "Periods 9-10 Passing",
-                },
-                {
-                    start: "14:54:00",
-                    end: "15:34:59",
-                    name: "Period 10",
-                },
-                {
-                    start: "15:35:00",
-                    end: "23:59:59",
-                    name: "After School",
-                },
-            ],
+            schedule: schedule,
+            periods_only: schedule.filter((v) => !v.name.includes("Passing")),
         };
     },
     mounted() {
@@ -153,6 +173,7 @@ export default {
         }, 1000);
 
         this.updateDiffs();
+        console.log(JSON.parse(JSON.stringify(this.periods_only)));
     },
     destroyed() {
         clearInterval(this.interval);
@@ -168,6 +189,13 @@ export default {
                 current_seconds,
                 this.schedule
             );
+            const seconds_elapsed =
+                current_seconds -
+                timeToSeconds(this.schedule[this.period_index].start);
+            const seconds_remaining =
+                timeToSeconds(this.schedule[this.period_index].end) -
+                current_seconds;
+            console.log(seconds_elapsed, seconds_remaining);
         },
     },
 };
@@ -178,5 +206,18 @@ export default {
     width: 100%;
     height: 100%;
     /* border: 1px solid black; */
+}
+#time_table {
+    border: 1px solid black;
+    width: 100%;
+}
+#time_table,
+td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+
+.current_period {
+    background-color: #cbcddf;
 }
 </style>
