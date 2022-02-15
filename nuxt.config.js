e846e90ddf9870b10a516835e037b11ea1c4a125
leomet07 <leomet07@gmail.com>
2022-02-15 15:12:59 -0500

    Title update

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
     // Global page headers: https://go.nuxtjs.dev/config-head
     head: {
-        title: "AndyFinCaucus",
+        title: "Freshman Caucus",
         htmlAttrs: {
             lang: "en",
         },
