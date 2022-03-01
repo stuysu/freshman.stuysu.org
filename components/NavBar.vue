62f9f24559bf236539ed84a801dfe367d8e2b461
leomet07 <leomet07@gmail.com>
2022-03-01 12:40:05 -0500

    Change dropdown burger color based on theme

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -31,7 +31,6 @@
                         width="384"
                         height="384"
                         viewBox="0 0 24 24"
-                        style="fill: #000000"
                     >
                         <path
                             d="M 2 5 L 2 7 L 22 7 L 22 5 L 2 5 z M 2 11 L 2 13 L 22 13 L 22 11 L 2 11 z M 2 17 L 2 19 L 22 19 L 22 17 L 2 17 z"
@@ -162,6 +161,7 @@ a {
     #burger_button_svg {
         height: 50px;
         width: 50px;
+        fill: var(--text-primary);
     }
 
     #top .title {
